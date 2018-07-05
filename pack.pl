d8036326897d8a113eec63abc9a7f47143dd5ed9
Wouter Beek <wouter@triply.cc>
2018-07-05 13:15:04 +0200

    DEPLOYMENT: Changed package name to legal Prolog Pack name.

--- pack.pl
+++ pack.pl
@@ -1,5 +1,5 @@
 home('https://github.com/wouterbeek/RDF-Tabling').
 keywords([entailment,'Linked Data','RDF','Semantic Web',tabling]).
-name('RDF-Tabling').
-title('An RDF(S) reasoner implemented with mode-directed tabling.').
+name(rdf_tabling).
+title('RDF-Tabling: An RDF(S) reasoner implemented with mode-directed tabling.').
 version('0.0.1')
