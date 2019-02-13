a90f0eeec33fdbd86a6f691d4f2a51e38b0cabfd
Wouter Beek <wouter@triply.cc>
2019-02-13 22:01:44 +0100

    CLEAN: Rename and overall repo cleanup.

--- prolog/semweb/rdf_backward.pl
+++ prolog/semweb/rdf_backward.pl
@@ -1,55 +1,71 @@
 :- encoding(utf8).
 :- module(
-  rdf_tabling,
+  rdf_backward,
   [
-    rdf_proof/1,      % ?Conclusion
-    rdf_proof_tree/1, % ?Conclusion
-    rdf_proof_tree/2  % ?Conclusion, -Proof
+    rdf_proof/1, % ?Conclusion
+    rdf_proof/2, % ?Conclusion, -Proof
+    rdf_prove/1  % ?Conclusion
   ]
 ).
 
 /** <module> RDF(S) entailment through mode-directed tabling
 
 @author Wouter Beek
-@version 2018
+@version 2018-2019
 */
 
 :- use_module(library(apply)).
 :- use_module(library(lists)).
-:- use_module(library(settings)).
+:- use_module(library(solution_sequences)).
+:- use_module(library(when)).
 
+:- use_module(library(proof)).
+:- use_module(library(semweb/rdf_api)).
 :- use_module(library(semweb/rdf_mem)).
 :- use_module(library(semweb/rdf_prefix)).
-:- use_module(library(semweb/rdf_proof_export)).
+:- use_module(library(semweb/rdf_proof)).
 :- use_module(library(semweb/rdf_term)).
-
-:- dynamic
-    axiom/2,
-    recognized_datatype_iri/1,
-    rule/3.
-
-:- multifile
-    axiom/2,
-    recognized_datatype_iri/1,
-    rule/3,
-    prolog:message//1.
 
 :- rdf_meta
    axiom(?, t),
    rdf_proof(t),
-   rdf_proof_tree(t),
-   rdf_proof_tree(t, -),
+   rdf_proof(t, -),
+   rdf_prove(t),
    recognized_datatype_iri(r),
    rule(?, t, t).
 
-:- setting(stage, oneof([setup,reasoning]), setup,
-           "The stage in which the reasoner is currently.").
-
 :- table
-   rdf_proof_/1,
-   rdf_proof_(_,lattice(shortest_proof)).
+   rdf_proof(_,lattice(shortest_proof)).
 
 
+
+
+
+%! rdf_prove(++Conclusion:compound) is semidet.
+%! rdf_prove(+Conclusion:compound) is nondet.
+%! rdf_proof(++Conclusion:compound) is semidet.
+%! rdf_proof(+Conclusion:compound) is nondet.
+%! rdf_proof(-Conclusion:compound) is multi.
+
+rdf_proof(Concl) :-
+  rdf_proof(Concl, Proof),
+  rdf_view_proof(Proof).
+
+
+%! rdf_proof(++Conclusion:compound, -Proof:compound) is semidet.
+%! rdf_proof(+Conclusion:compound, -Proof:compound) is nondet.
+%! rdf_proof(-Conclusion:compound, -Proof:compound) is multi.
+
+rdf_proof(Concl, t(Rule,Concl,Proofs)) :-
+  rule(Rule, Concl, Prems),
+  maplist(rdf_proof, Prems, Proofs).
+
+
+
+%! rdf_prove(-Conclusion:compound) is multi.
+
+rdf_prove(Concl) :-
+  distinct(Concl, rdf_proof(Concl, _)).
 
 
 
@@ -64,8 +80,8 @@ axiom(rdf,  rdf(rdf:rest,           rdf:type,           rdf:'Property'  )).
 axiom(rdf,  rdf(rdf:value,          rdf:type,           rdf:'Property'  )).
 axiom(rdf,  rdf(rdf:nil,            rdf:type,           rdf:'List'      )).
 axiom(rdf,  rdf(P,                  rdf:type,           rdf:'Property'  )) :-
-  rdf_check_predicate(P),
-  rdf_container_membership_property(_, P, _).
+  when(ground(P), rdf_is_predicate(P)),
+  rdf_container_membership_property(P).
 axiom(rdfs, rdf(rdf:type,           rdfs:domain,        rdfs:'Resource' )).
 axiom(rdfs, rdf(rdfs:domain,        rdfs:domain,        rdf:'Property'  )).
 axiom(rdfs, rdf(rdfs:range,         rdfs:domain,        rdf:'Property'  )).
@@ -105,14 +121,14 @@ axiom(rdfs, rdf(rdfs:'ContainerMembershipProperty', rdfs:subClassOf, rdf:'Proper
 axiom(rdfs, rdf(rdfs:isDefinedBy,   rdfs:subPropertyOf, rdfs:seeAlso    )).
 axiom(rdfs, rdf(rdfs:'Datatype',    rdfs:subClassOf,    rdfs:'Class'    )).
 axiom(rdfs, rdf(P,                  rdf:type,           rdfs:'ContainerMembershipProperty')) :-
-  rdf_check_predicate(P),
-  rdf_container_membership_property(_, P, _).
+  when(ground(P), rdf_is_predicate(P)),
+  rdf_container_membership_property(P).
 axiom(rdfs, rdf(P,                  rdfs:domain,        rdfs:'Resource' )) :-
-  rdf_check_predicate(P),
-  rdf_container_membership_property(_, P, _).
+  when(ground(P), rdf_is_predicate(P)),
+  rdf_container_membership_property(P).
 axiom(rdfs, rdf(P,                  rdfs:range,         rdfs:'Resource' )) :-
-  rdf_check_predicate(P),
-  rdf_container_membership_property(_, P, _).
+  when(ground(P), rdf_is_predicate(P)),
+  rdf_container_membership_property(P).
 
 
 
@@ -136,8 +152,8 @@ recognized_datatype_iri(xsd:string).
 %! rule(?Rule:compound, ?Conclusion:compound, -Premises:list(compound)) is nondet.
 
 rule(B,            rdf(S,P,O),                             []) :-
-  rdf_check_subject(S),
-  rdf_check_predicate(P),
+  when(ground(S), rdf_is_subject(S)),
+  when(ground(P), rdf_is_predicate(P)),
   tp(B, S, P, O).
 rule(axiom(Vocab), Concl,                                  []) :-
   axiom(Vocab, Concl).
@@ -162,110 +178,3 @@ rule(rdfs(10),     rdf(C,rdfs:subClassOf,C),               [rdf(C,rdf:type,rdfs:
 rule(rdfs(11),     rdf(C,rdfs:subClassOf,E),               [rdf(C,rdfs:subClassOf,D),rdf(D,rdfs:subClassOf,E)]).
 rule(rdfs(12),     rdf(P,rdfs:subPropertyOf,rdfs:member),  [rdf(P,rdf:type,rdfs:'ContainerMembershipProperty')]).
 rule(rdfs(13),     rdf(C,rdfs:subClassOf,rdfs:'Literal'),  [rdf(C,rdf:type,rdfs:'Datatype')]).
-
-
-
-
-
-/* REASONING STAGE
-
-Implementation of the reasoner itself.  Invoking one of the prove
-predicates automatically end setup stage and enters reasoning stage.
-*/
-
-%! rdf_proof(++Conclusion:compound) is semidet.
-%! rdf_proof(+Conclusion:compound) is nondet.
-%! rdf_proof(-Conclusion:compound) is multi.
-
-rdf_proof(Concl) :-
-  set_stage(reasoning),
-  rdf_proof_(Concl).
-
-rdf_proof_(Concl) :-
-  rule(_Rule, Concl, Prems),
-  maplist(rdf_proof_, Prems).
-
-
-
-%! rdf_proof_tree(++Conclusion:compound) is semidet.
-%! rdf_proof_tree(+Conclusion:compound) is nondet.
-%! rdf_proof_tree(-Conclusion:compound) is multi.
-
-rdf_proof_tree(Concl) :-
-  rdf_proof_tree(Concl, Proof),
-  view_proof(Proof).
-
-
-%! rdf_proof_tree(++Conclusion:compound, -Proof:compound) is semidet.
-%! rdf_proof_tree(+Conclusion:compound, -Proof:compound) is nondet.
-%! rdf_proof_tree(-Conclusion:compound, -Proof:compound) is multi.
-
-rdf_proof_tree(Concl, Proof) :-
-  set_stage(reasoning),
-  rdf_proof_(Concl, Proof).
-
-rdf_proof_(Concl, t(Rule,Concl,Trees)) :-
-  rule(Rule, Concl, Prems),
-  maplist(rdf_proof_, Prems, Trees).
-
-shortest_proof(Tree1, Tree2, Tree) :-
-  maplist(tree_depth, [Tree1,Tree2], [Depth1,Depth2]),
-  (Depth2 < Depth1 -> Tree = Tree2 ; Tree = Tree1).
-
-
-
-
-
-% HELPERS %
-
-%! assume_setup_stage(+Predicate:compound) is det.
-%
-% @throws error(stage(reasoning),Context)
-
-assume_setup_stage(Pred/Arity) :-
-  setting(stage, reasoning), !,
-  throw(error(stage(reasoning),Pred/Arity)).
-assume_setup_stage(_).
-
-prolog:message(error(stage(Stage),Pred/Arity)) -->
-  ["Call to ‘~a/~d’ not allowed in ~a stage."-[Pred,Arity,Stage]].
-
-
-
-%! rdf_check_predicate(?P:atom) is semidet.
-
-rdf_check_predicate(P) :-
-  var(P), !.
-rdf_check_predicate(P) :-
-  rdf_is_predicate(P).
-
-
-
-%! rdf_check_subject(?S:atom) is semidet.
-
-rdf_check_subject(S) :-
-  var(S), !.
-rdf_check_subject(S) :-
-  rdf_is_subject(S).
-
-
-
-%! set_stage(+Stage:oneof([reasoning])) is det.
-
-set_stage(Stage) :-
-  setting(stage, Stage), !.
-set_stage(reasoning) :-
-  set_setting(stage, reasoning).
-set_stage(setup) :-
-  abolish_all_tables,
-  setting(stage, reasoning), !.
-
-
-
-%! tree_depth(+Tree:compound, -Depth:nonneg) is det.
-
-tree_depth(t(_,_,[]), 0) :- !.
-tree_depth(t(_,_,Trees), Depth) :-
-  maplist(tree_depth, Trees, Depths),
-  max_list(Depths, Depth0),
-  Depth is Depth0 + 1.
